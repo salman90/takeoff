3aca3ec3e68178797322720293102b376dca4a22
Mahan Esmaeilzadeh <mesmaeilzadeh@my.bcit.ca>
2021-08-03 15:23:07 -0700

    current status

--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import app from './reducers/app'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
+ 
 export default function configureStore() {
     return createStore(
         app,
