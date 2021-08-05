e86d54f20424d887f9067e3c23ee5353485f34df
Mahan Esmaeilzadeh <mesmaeilzadeh@my.bcit.ca>
2021-08-05 10:45:02 -0700

    add date selector field

---
+++ src/componets/DatePicker/Date.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Modal, Button, Form, Dropdown, InputGroup, FormControl } from 'react-bootstrap';
+import "react-datetime/css/react-datetime.css";
+import Datetime from "react-datetime";
+
+//taken from https://github.com/arqex/react-datetime
+
+export default function Date() {
+  return <Datetime />;
+}
--- src/componets/Flight-input/flight-input.jsx
+++ src/componets/Flight-input/flight-input.jsx
@@ -1,9 +1,13 @@
 import React, { PureComponent } from 'react';
-import { Modal, Button, Form, Dropdown } from 'react-bootstrap';
+import { Modal, Button, Form, Dropdown, InputGroup, FormControl } from 'react-bootstrap';
+import Date from '../DatePicker';
+
+
+
+
 
 
 //a class to handle the user's flights search in put parameters.
-
 class FligthsInput extends PureComponent {
 
     constructor(props) {
@@ -51,51 +55,65 @@ class FligthsInput extends PureComponent {
 
     gatherData = (e) => {
         console.log("gatherdata")
+        console.log(this.props)
+        console.log(this.state)
         this.props.flightSearch(this.state.maxPrice, this.state.passport, this.state.startDate, this.state.endDate, this.state.currentCity);
     }
     render() {
-        const blogs = ["Iran", "Dubai"]
+        // const blogs = ["Iran", "Dubai"]
 
         console.log("I AM HERE!")
         return (<>
+            <h2>Let's find you a flight!</h2>
             <Form>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Passport</Form.Label>
-                    <Form.Control size="sm" type="text" onChange={this.passport} placeholder="Passport" />
+                    <Form.Control type="text" onChange={this.passport} placeholder="Passport" />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Label>Current City</Form.Label>
+                    <Form.Control type="text" onChange={this.currentCity} placeholder="City of Origin" />
                 </Form.Group>
+                <Form.Label>Budget</Form.Label>
+                <InputGroup className="mb-3">
+                    <InputGroup.Text>$</InputGroup.Text>
+                    <FormControl aria-label="Amount (to the nearest dollar)" onChange={this.maxPrice} placeholder="Max Budget" />
+                </InputGroup>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group>
-                <Button variant="primary" onClick={()=> {this.gatherData()}}>Submit</Button>
+                <Button variant="primary" onClick={() => { this.gatherData() }}>Submit</Button>
             </Form>
-            <div className="dropdown">
-                <Dropdown>
-                    <Dropdown.Toggle
-                        variant="secondary btn-sm"
-                        id="dropdown-basic">
-                        Language
-                    </Dropdown.Toggle>
+            <Date/>
 
-                    <Dropdown.Menu style={{ backgroundColor: '#73a47' }}>
-                        <Dropdown.Item href="#" >Arabic</Dropdown.Item>
-                        <Dropdown.Item href="#">English</Dropdown.Item>
-                        {blogs.map((blog) => (
-                            <div className="blog-preview" key={blog.id}>
-                                <Dropdown.Item href="#">{blog}</Dropdown.Item>
-                                {console.log(this.props)}
-                            </div>
 
-                        ))}
-                    </Dropdown.Menu>
-                </Dropdown>
-            </div>
         </>)
     }
 
 }
 
 export default FligthsInput;
+
+
+
+{/* <div className="dropdown">
+<Dropdown>
+    <Dropdown.Toggle
+        variant="secondary btn-sm"
+        id="dropdown-basic">
+        Language
+    </Dropdown.Toggle>
+
+    <Dropdown.Menu style={{ backgroundColor: '#73a47' }}>
+        <Dropdown.Item href="#" >Arabic</Dropdown.Item>
+        <Dropdown.Item href="#">English</Dropdown.Item>
+        {/* {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+                <Dropdown.Item href="#">{blog}</Dropdown.Item>
+                {console.log(this.props)}
+            </div>
+
+        ))} */}
+//     </Dropdown.Menu>
+// </Dropdown>
+// </div> */}
